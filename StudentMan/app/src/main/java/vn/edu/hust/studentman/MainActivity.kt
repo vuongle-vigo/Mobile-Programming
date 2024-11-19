548d0e9904263212876e88aeb70c3d35b4bd5f5b
Vigo0x1 <vuongle.vigo@gmail.com>
2024-11-19 23:43:50 +0700

    Push code studentmain assignment

---
+++ StudentMan/app/src/main/java/vn/edu/hust/studentman/MainActivity.kt
@@ -0,0 +1,47 @@
+package vn.edu.hust.studentman
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val students = mutableListOf(
+      StudentModel("Nguyễn Văn An", "SV001"),
+      StudentModel("Trần Thị Bảo", "SV002"),
+      StudentModel("Lê Hoàng Cường", "SV003"),
+      StudentModel("Phạm Thị Dung", "SV004"),
+      StudentModel("Đỗ Minh Đức", "SV005"),
+      StudentModel("Vũ Thị Hoa", "SV006"),
+      StudentModel("Hoàng Văn Hải", "SV007"),
+      StudentModel("Bùi Thị Hạnh", "SV008"),
+      StudentModel("Đinh Văn Hùng", "SV009"),
+      StudentModel("Nguyễn Thị Linh", "SV010"),
+      StudentModel("Phạm Văn Long", "SV011"),
+      StudentModel("Trần Thị Mai", "SV012"),
+      StudentModel("Lê Thị Ngọc", "SV013"),
+      StudentModel("Vũ Văn Nam", "SV014"),
+      StudentModel("Hoàng Thị Phương", "SV015"),
+      StudentModel("Đỗ Văn Quân", "SV016"),
+      StudentModel("Nguyễn Thị Thu", "SV017"),
+      StudentModel("Trần Văn Tài", "SV018"),
+      StudentModel("Phạm Thị Tuyết", "SV019"),
+      StudentModel("Lê Văn Vũ", "SV020")
+    )
+
+    val studentAdapter = StudentAdapter(students)
+
+    findViewById<RecyclerView>(R.id.recycler_view_students).run {
+      adapter = studentAdapter
+      layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+  }
+}
